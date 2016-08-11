4375e4ea81a6cd1f9bf68f6b9774fdf6e4b83941
Olga Lozovik <lozovikolga@gmail.com>
2016-08-11 19:31:06 +0300

    Task 2 done.

--- Task2/scripts/init.js
+++ Task2/scripts/init.js
@@ -2,20 +2,17 @@ function init() {
 	var array = []; 
 	for (var i = 0; i < 5; i++) {
 		array[i] = createElement();
-	}
-	
+	}	
 	logData(array);
 }
 
 function createElement() {
 	var type = random(1, 3);
 	var element = {};
-
 	element.count = random(1, 10);
 	element["getCount" + type] = function() {
 		return this.count;
-	};
-	
+	};	
 	return element;
 }
 
--- Task2/scripts/log.js
+++ Task2/scripts/log.js
@@ -2,13 +2,11 @@ function logData(data) {
 	for (var i = 1; i <= 3; i++) {
 		var count = 0;
 		var method = "getCount" + i;
-		
 		for (var j = 0; j < data.length; j++) {
 			if (method in data[j]) {
 				count += data[j][method]();
 			}
-		}		
-		
+		}				
 		console.log("count{%s}=%d", i, count);
 	}
 }

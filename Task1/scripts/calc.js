dc9de8c07b78e566d20ce2d40557670318b5a5bb
Olga Lozovik <lozovikolga@gmail.com>
2016-08-11 16:21:33 +0300

    Small changes in formatting; remove defer.

--- Task1/scripts/calc.js
+++ Task1/scripts/calc.js
@@ -3,13 +3,13 @@ for (var i = 0; i < data.length; i++) {
 		if (data[i] == 0) {
 			data[i] = +data[i] + 10;
 		}
-		else
-		if (data[i] < 100) {
+		else if (data[i] < 100) {
 			data[i] = +data[i] + 100;
 		}
-		else
-		if (data[i] > 100) {
+		else if (data[i] > 100) {
 			data[i] -= 100;
 		}
 	}
-};
+}
+
+logData(data);

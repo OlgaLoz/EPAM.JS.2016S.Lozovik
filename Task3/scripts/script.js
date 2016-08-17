81c56528d3f6c3310a04669efd052c7a8f2b7986
Olga Lozovik <lozovikolga@gmail.com>
2016-08-17 18:26:13 +0300

    Fixed task 3.

--- Task3/scripts/script.js
+++ Task3/scripts/script.js
@@ -1,52 +1,60 @@
-function disableButton(name) {
-    $(name).attr("disabled", true);
-    $(name).addClass("disabled-button");
-}
-
-function enableButton(name) {
-    $(name).attr("disabled", false);
-    $(name).removeClass("disabled-button");
-}
-
 $(document).ready(function() {
     var divCount = 160;
     var numbers = [];
 
-    disableButton("#colorBtn");
-    disableButton("#resetBtn");
+    touggleButton("#colorBtn");
+    touggleButton("#resetBtn");
 
     $("#generateBtn").click(function() {
     	var divs = "";
-        numbers = createArray(divCount);
-
-    	for (var i = 0; i < divCount; i++) {
-    		divs += "<div class=\"small-block\">" + numbers[i].number + "</div>";
-    	}   
+        for (var i = 0; i < divCount; i++) {
+            numbers[i] = random(1,100);
+            divs += "<div class=\"small-block\">" + numbers[i] + "</div>";
+        }
 
     	$(".content").append(divs);   
 
-        disableButton(this) 
-        enableButton("#colorBtn");
-        enableButton("#resetBtn");
+        touggleButton(this) 
+        touggleButton("#colorBtn");
+        touggleButton("#resetBtn");
     });
 
     $("#colorBtn").click(function() {
-        setColors(numbers);
-
         $(".content .small-block").each(function(i) {
-            $(this).css("background-color", numbers[i].color);
+            $(this).addClass(getColor(numbers[i]));
         });
 
-        disableButton(this);
+        touggleButton(this);
     });
 
     $("#resetBtn").click(function() {
         $(".content").empty();
-
         numbers = [];
 
-        disableButton("#colorBtn");
-        disableButton(this);
-        enableButton("#generateBtn");
+        if (!$("#colorBtn").hasClass("disabled")) {
+            touggleButton("#colorBtn");
+        }       
+        touggleButton(this);
+        touggleButton("#generateBtn");
     });
+
+    function touggleButton(name) {
+        $(name).toggleClass("disabled");
+    }
+
+    function random(min, max) {
+        return Math.floor((Math.random() * max) + min);
+    }
+
+    function getColor(value) {
+        if (value > 75) {
+            return "color-red";
+        } 
+        else if (value > 50) {
+            return "color-orange";
+        } 
+        else if (value > 25) {
+            return "color-green";
+        }
+    }
 });
